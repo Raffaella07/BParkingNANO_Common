65a599798d8f6ddc8eef5117f7aafad8a7451d8c
anlyon <alyon@phys.ethz.ch>
2020-11-09 21:02:42 +0100

    nanoMerger adapted for flat merging

--- BParkingNano/test/nanoMerger.py
+++ BParkingNano/test/nanoMerger.py
@@ -9,17 +9,11 @@ def getOptions():
   parser = ArgumentParser(description='Script to merge the nanoAOD files resulting from a multijob production', add_help=True)
   parser.add_argument('--pl', type=str, dest='pl', help='label of the sample file', default='V01_n9000000_njt300')
   parser.add_argument('--tag', type=str, dest='tag', help='[optional] tag to be added on the outputfile name', default=None)
+  parser.add_argument('--doflat', dest='doflat', help='merge as well the files coming out of the truth-mathing', action='store_true', default=False)
   return parser.parse_args()
 
 
-if __name__ == "__main__":
-
-  opt = getOptions()
-
-  user      = os.environ["USER"]
-
-  locationSE = '/pnfs/psi.ch/cms/trivcat/store/user/{}/BHNLsGen/{}/'.format(user, opt.pl)
-  nanoName = '/nanoFiles/bparknano_nj*.root' if opt.tag == None else '/nanoFiles/bparknano_{}_nj*.root'.format(opt.tag)
+def doMerging(nanoName, mergedName, outputdir, cond):
 
   print '\n-> Getting the different mass points'
   pointsdir = [f for f in glob.glob(locationSE+'/*')]
@@ -31,30 +25,52 @@ if __name__ == "__main__":
     nanoFiles = [f for f in glob.glob(point+nanoName)]
 
     # create the outputdir that will contain the merged file
-    outputdir = '{}/nanoFiles/merged'.format(point)
+    outputdir = point + outputdir
     os.system('mkdir {}'.format(outputdir))
 
     filesValid = []
     print "\n-> Checking the files"
     for fileName in nanoFiles:
-       rootFile = ROOT.TNetXNGFile.Open(fileName, 'r')
-       if rootFile and rootFile.GetListOfKeys().Contains('Events'):
+      rootFile = ROOT.TNetXNGFile.Open(fileName, 'r')
+      if rootFile: 
+        if cond:
+          if rootFile.GetListOfKeys().Contains('Events'):
+            filesValid.append(fileName)
+        else:
           filesValid.append(fileName)
 
     print '\n-> Start of the merge'
-    mergedName = 'bparknano.root' if opt.tag == None else 'bparknano_{}.root'.format(opt.tag)
     command = 'python haddnano.py {}/{}'.format(outputdir, mergedName)
-    for fileName in filesValid:
+    for iFile, fileName in enumerate(filesValid):
+       #if iFile > 2: continue
        command = command + ' {}'.format(fileName)
 
     os.system(command)
 
+  print '{}/{} created'.format(outputdir, mergedName)
+
+
+if __name__ == "__main__":
+
+  opt = getOptions()
+
+  user      = os.environ["USER"]
+
+  locationSE = '/pnfs/psi.ch/cms/trivcat/store/user/{}/BHNLsGen/{}/'.format(user, opt.pl)
+ 
+  nanoName   = '/nanoFiles/bparknano_nj*.root' if opt.tag == None else '/nanoFiles/bparknano_{}_nj*.root'.format(opt.tag)
+  mergedName = 'bparknano.root' if opt.tag == None else 'bparknano_{}.root'.format(opt.tag)
+  outputdir  = '/nanoFiles/merged'
+  
+  nanoName_flat   = '/nanoFiles/flat/flat_bparknano_nj*.root' if opt.tag == None else '/nanoFiles/flat/flat_bparknano_{}_nj*.root'.format(opt.tag)
+  mergedName_flat = 'flat_bparknano.root' if opt.tag == None else 'flat_bparknano_{}.root'.format(opt.tag)
+  outputdir_flat  = '/nanoFiles/flat/merged'
+
+  doMerging(nanoName, mergedName, outputdir, True)
+  if opt.doflat:
+    doMerging(nanoName_flat, mergedName_flat, outputdir_flat, False)
+
   print 'Done'
-
-
-
-
-
 
 
 
