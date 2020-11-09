2a7cdc58680ebf14c48dfe016299e3b6a2177739
anlyon <alyon@phys.ethz.ch>
2020-11-09 17:34:39 +0100

    pion impact parameters added

--- BParkingNano/test/flat_tree_branches.py
+++ BParkingNano/test/flat_tree_branches.py
@@ -7,7 +7,6 @@ branches['run'           ]     = lambda x : x.run
 branches['lumi'          ]     = lambda x : x.lumi
 branches['event'         ]     = lambda x : x.event
 
-
 branches['b_pt'          ]     = lambda x : x.b.pt()
 branches['b_eta'         ]     = lambda x : x.b.eta()
 branches['b_phi'         ]     = lambda x : x.b.phi()
@@ -38,7 +37,11 @@ branches['pi_pt'         ]     = lambda x : x.b.pi().pt()
 branches['pi_eta'        ]     = lambda x : x.b.pi().eta()
 branches['pi_phi'        ]     = lambda x : x.b.pi().phi()
 branches['pi_mass'       ]     = lambda x : x.b.pi().mass()
-#branches['pi_dz'         ]     = lambda x : x.ProbeTracks_dz()
+branches['pi_dz'         ]     = lambda x : x.b.pi_dz()
+branches['pi_dxy'        ]     = lambda x : x.b.pi_dxy()
+branches['pi_dzS'        ]     = lambda x : x.b.pi_dzS()
+branches['pi_dxyS'       ]     = lambda x : x.b.pi_dxyS()
+branches['pi_DCASig'     ]     = lambda x : x.b.pi_DCASig()
 
 branches['trg_mu_pt'     ]     = lambda x : x.b.trg_mu().pt()
 branches['trg_mu_eta'    ]     = lambda x : x.b.trg_mu().eta()
@@ -50,11 +53,8 @@ branches['trg_mu_dxy'    ]     = lambda x : x.b.trg_mu_dxy()
 branches['trg_mu_dz'     ]     = lambda x : x.b.trg_mu_dz()
 
 # difference of vertex positions of the two muons
-#branches['muons_vxdiff'  ]     = lambda x : x.b.mu_dx()
 branches['muons_vxdiff'  ]     = lambda x : x.b.mu_vxdiff()
-#branches['muons_vydiff'  ]     = lambda x : x.b.mu_dy()
 branches['muons_vydiff'  ]     = lambda x : x.b.mu_vydiff()
-#branches['muons_vzdiff'  ]     = lambda x : x.b.mu_dz()
 branches['muons_vzdiff'  ]     = lambda x : x.b.mu_vzdiff()
 branches['muons_Lxy'     ]     = lambda x : x.b.mu_Lxy()
 branches['muons_Lxyz'    ]     = lambda x : x.b.mu_Lxyz()
@@ -67,8 +67,5 @@ branches['dr_mu_pi'      ]     = lambda x : x.b.dr_mu_pi()
 branches['dr_trgmu_hnl'  ]     = lambda x : x.b.dr_trgmu_hnl()
 
 
-
-#branches['Muon_softId'] = lambda x : x.Muon_softId
-#branches['Muon_softId'] = lambda x : x.Muon_softId
 
 
