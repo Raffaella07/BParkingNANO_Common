595bf24b2a6377b2f176073009a3ead788586b3a
anlyon <alyon@phys.ethz.ch>
2021-02-01 13:16:56 +0100

    material to create run_tree added

--- BParkingNano/plugins/dumper/starter.C
+++ BParkingNano/plugins/dumper/starter.C
@@ -2,15 +2,20 @@
 #include <iostream>
 #include "TProof.h"
 
-void starter(){
-
+void starter(TString isMC){
   TString inFileName = "bparknano.root";
 
   TString outFileName = "flat_bparknano.root";
 
-  TChain * c = new TChain("Events");
+  TChain* c = new TChain("Events");
   c->Add(inFileName);
 
-  c->Process("NanoDumper.C", outFileName);
+  c->Process("NanoDumper.C+", outFileName);
 
+  if(isMC == "true"){
+    TChain* c_run = new TChain("Runs");
+    c_run->Add(inFileName);
+
+    c_run->Process("NanoRunDumper.C+", outFileName);
+  }
 }
