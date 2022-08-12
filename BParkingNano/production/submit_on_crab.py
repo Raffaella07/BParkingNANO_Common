57637de472f23ee1fdbf7af1db0bda181106966f
Raffaella07 <raffaella.tram@gmail.com>
2022-08-12 10:28:34 +0200

    run range splitting added for data production

--- BParkingNano/production/submit_on_crab.py
+++ BParkingNano/production/submit_on_crab.py
@@ -1,9 +1,15 @@
 from CRABClient.UserUtilities import config, ClientException, getUsernameFromCRIC
 #from input_crab_data i  mport dataset_files
-import yaml              
+import yaml        
+import numpy as np      
 import datetime
 from fnmatch import fnmatch
 from argparse import ArgumentParser
+from FWCore.PythonUtilities.LumiList import LumiList
+import CRABClient
+from dbs.apis.dbsClient import DbsApi
+dbs = DbsApi('https://cmsweb.cern.ch/dbs/prod/global/DBSReader')
+
 
 production_tag = datetime.date.today().strftime('%Y%b%d')
 
@@ -67,43 +73,57 @@ if __name__ == '__main__':
         # filter names according to what we need
           if not fnmatch(name, args.filter):
 		 continue
-          print 'submitting', name
-  
-          isMC = info['isMC']
-  
-          config.Data.inputDataset = info['dataset'] % part \
-                                     if part is not None else \
-                                        info['dataset']
-  
-          config.General.requestName = name
+	  
+	  isMC = info['isMC']
           common_branch = 'mc' if isMC else 'data'
-          config.Data.splitting = 'FileBased'# if isMC else 'Automatic'
-          if not isMC:
-              config.Data.lumiMask = info.get(
-                  'lumimask', 
-                  common[common_branch].get('lumimask', None)
-              )	
-  	 #   config.Data.runRange = '315257-319914'
-          else:
-              config.Data.lumiMask = ''
-  
-          config.Data.unitsPerJob = info.get(
-              'splitting',
-              common[common_branch].get('splitting', None)
-          )
-          globaltag = info.get(
-              'globaltag',
-              common[common_branch].get('globaltag', None)
-          )
-          
-          config.JobType.pyCfgParams = [
-              'isMC=%s' % isMC, 'reportEvery=1000',
-              'tag=%s' % production_tag,
-              'globalTag=%s' % globaltag,
-          ]
-          
-          config.JobType.outputFiles = ['_'.join(['BParkNANO', 'mc' if isMC else 'data', production_tag])+'.root']
-          
-          print config
-          submit(config)
+	  config.Data.unitsPerJob = info.get(
+	      'splitting',
+	      common[common_branch].get('splitting', None)
+	  )
+	  globaltag = info.get(
+	      'globaltag',
+	      common[common_branch].get('globaltag', None)
+	  )
+	  
+	  config.JobType.pyCfgParams = [
+	      'isMC=%s' % isMC, 'reportEvery=1000',
+	      'tag=%s' % production_tag,
+	      'globalTag=%s' % globaltag,
+	  ]
+          config.Data.splitting = 'FileBased' if isMC else 'Automatic'
+	          
+	  config.JobType.outputFiles = ['_'.join(['BParkNANO', 'mc' if isMC else 'data', production_tag])+'.root']
+	  
+	  config.Data.inputDataset = info['dataset'] % part \
+	                             if part is not None else \
+	                                info['dataset']
+	  runs= [run['run_num'] for run in dbs.listRuns(dataset = config.Data.inputDataset)]
+	     # print(runs)
+          splits = np.array_split(runs,4)
+	  print(splits)
+	  if not isMC:
+	 	 for i in range(1,len(splits)):
+	  	
+		          config.General.requestName = name+"_section"+str(i) if len(splits)!=0 else sample
+		          print 'submitting', config.General.requestName
+			  lumiList = LumiList(filename=info.get(
+		             'lumimask', 
+		              common[common_branch].get('lumimask', None))
+		       		)
+			      
+		          lumiList.selectRuns([x for x in splits[i]])
+			  lumiList.writeJSON('lumi_mask_task_'+str(i)+'.json')
+		          config.Data.lumiMask = 'lumi_mask_task_'+str(i)+'.json'
+		              	
+		  
+		          
+		          print config
+	 	          submit(config)
+	  else:	
+		config.General.requestName = name	
+		print 'submitting', config.General.requestName
+		common_branch = 'mc' if isMC else 'data'
+		config.Data.lumiMask = ''
+		print config
+	 	submit(config)
   
